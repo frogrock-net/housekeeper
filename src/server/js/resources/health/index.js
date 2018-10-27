f6124abf07426ccb55bbe86cbc40227dcfa07f52
alisazhou <alisa.zwy@gmail.com>
2018-10-26 22:55:57 -0700

    add HealthModel to health check

--- src/server/js/resources/health/index.js
+++ src/server/js/resources/health/index.js
@@ -1,11 +1,23 @@
 import express from 'express';
+
+import HealthModel from './health';
+
 const router = new express.Router();
 
 /**
  * Basic health-check endpoint.
  *
- * Returns 'hello, world.' when accessed.
+ * Returns 'hello, world.' plus time when accessed.
  */
-router.get('/', (req, res) => res.send('hello, world.'));
+router.get('/', (req, res) => {
+    const healthCheck = new HealthModel({});
+    healthCheck.save(err => {
+        if (err) {
+            return console.error(err);
+        }
+    });
+
+    res.send(`hello, world.  ${healthCheck.checkedOn}`);
+});
 
 export default router;
