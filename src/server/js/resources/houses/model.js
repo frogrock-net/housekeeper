52b5518ba0199b4dc28182190b86212819f35987
alisazhou <alisa.zwy@gmail.com>
2018-10-30 20:18:47 -0700

    add validator package

--- src/server/js/resources/houses/model.js
+++ src/server/js/resources/houses/model.js
@@ -1,11 +1,19 @@
 import mongoose from 'mongoose';
+import validator from 'validator';
+import { partialRight } from 'lodash';
 
 const HouseSchema = new mongoose.Schema({
     address: {
         street: String,
         city: String,
         state: String,
-        zip: String,
+        zip: {
+            type: String,
+            validate: {
+                validator: partialRight(validator.isPostalCode, 'US'),
+                message: props => `${props.value} is not a valid US zipcode`,
+            },
+        },
     },
     name: { type: String, required: true },
     rooms: [
--- src/server/js/resources/users/model.js
+++ src/server/js/resources/users/model.js
@@ -1,9 +1,19 @@
 import mongoose from 'mongoose';
+import validator from 'validator';
 
 const UserSchema = new mongoose.Schema({
     firstName: { type: String, required: true, lowercase: true, trim: true },
     lastName: { type: String, required: true, lowercase: true, trim: true },
-    email: { type: String, required: true, lowercase: true, trim: true },
+    email: {
+        type: String,
+        required: true,
+        lowercase: true,
+        trim: true,
+        validate: {
+            validator: validator.isEmail,
+            message: props => `${props.value} is not a valid email address`,
+        },
+    },
 });
 
 export default mongoose.model('UserModel', UserSchema);
