c05fa4dcd6c63114c1466eb06b86f24a53fccaa3
alisazhou <alisa.zwy@gmail.com>
2018-11-02 12:03:19 -0700

    add validation

--- src/server/js/resources/bookings/model.js
+++ src/server/js/resources/bookings/model.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import { values } from 'lodash';
+import moment from 'moment';
 
 const BOOKING_STATUSES = {
     APPROVED: 'APPROVED',
@@ -13,13 +14,29 @@ const BookingSchema = new mongoose.Schema({
         ref: 'UserModel',
         required: true,
     },
-    endDate: { type: Date, required: true },
+    endDate: {
+        type: Date,
+        required: true,
+        validate: {
+            validator: function(value) {
+                return moment(value).isAfter(this.startDate);
+            },
+            message: 'Start date must be after end date.',
+        },
+    },
     room: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'RoomModel',
         required: true,
     },
-    startDate: { type: Date, required: true },
+    startDate: {
+        type: Date,
+        required: true,
+        validate: {
+            validator: value => !moment().isAfter(value),
+            message: 'Start date cannot be before today.',
+        },
+    },
     status: {
         type: String,
         enum: values(BOOKING_STATUSES),
