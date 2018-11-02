4d5b7f1ed9ff31c1b9c9e93e959a2460a4f70834
alisazhou <alisa.zwy@gmail.com>
2018-11-02 14:46:46 -0700

    remove health from graphql, reorganize

--- src/server/js/schema.js
+++ src/server/js/schema.js
@@ -1,36 +1,39 @@
 import { merge } from 'lodash';
 import { makeExecutableSchema } from 'graphql-tools';
 
-import HealthModel from './resources/health/model';
-import { typeDefs as Health } from './resources/health/schema';
 import HouseModel from './resources/houses/model';
-import { typeDefs as House } from './resources/houses/schema';
+import * as houseSchema from './resources/houses/schema';
 import UserModel from './resources/users/model';
-import { typeDefs as User } from './resources/users/schema';
+import * as userSchema from './resources/users/schema';
 
 const queryTypeDefs = `
     type Query {
         allHouses: [House],
+        housesByAdministrator(administratorId: ID!): [House],
         allUsers: [User],
         userByEmail(email: String): User,
-        healthCheck: [Health]
     }
 `;
 
 const queryResolvers = {
     Query: {
         allHouses: (root, args, context, info) =>
-            HouseModel.find().populate('administrators'),
+            HouseModel.find().populate('administrators'), // TODO: remove populate?
+        housesByAdministrator: (root, args, context, info) =>
+            HouseModel.find({ administrators: args.administratorId }),
         allUsers: (root, args, context, info) => UserModel.find(),
         userByEmail: (root, args, context, info) =>
             UserModel.findOne({ email: args.email }),
-        healthCheck: (root, args, context, info) => HealthModel.find(),
     },
 };
 
 const schema = makeExecutableSchema({
-    typeDefs: [queryTypeDefs, House, User, Health],
-    resolvers: merge(queryResolvers),
+    typeDefs: [queryTypeDefs, houseSchema.typeDefs, userSchema.typeDefs],
+    resolvers: merge(
+        queryResolvers,
+        houseSchema.resolvers,
+        userSchema.resolvers
+    ),
 });
 
 export default schema;
