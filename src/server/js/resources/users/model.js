b1c28aeacbd15ce74b1de35060205b9b293a8f73
alisazhou <alisa.zwy@gmail.com>
2018-11-21 17:42:58 -0500

    add salt and hash to user model

--- src/server/js/resources/users/model.js
+++ src/server/js/resources/users/model.js
@@ -1,3 +1,4 @@
+import crypto from 'crypto';
 import mongoose from 'mongoose';
 import validator from 'validator';
 
@@ -7,6 +8,7 @@ const UserSchema = new mongoose.Schema({
         required: true,
         lowercase: true,
         trim: true,
+        unique: true,
         validate: {
             validator: validator.isEmail,
             message: props => `${props.value} is not a valid email address`,
@@ -14,8 +16,20 @@ const UserSchema = new mongoose.Schema({
     },
     firstName: { type: String, required: true, lowercase: true, trim: true },
     lastName: { type: String, required: true, lowercase: true, trim: true },
+    hash: String,
+    salt: String,
 });
 
 UserSchema.virtual('fullName').get(() => `${this.firstName} ${this.lastName}`);
+
+UserSchema.methods.setPassword = function(password) {
+    this.salt = crypto.randomBytes(16).toString('hex');
+    this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512').toString('hex');
+};
+
+UserSchema.methods.validatePassword = function(password) {
+    const hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512').toString('hex');
+    return this.hash === hash;
+};
 
 export default mongoose.model('UserModel', UserSchema);
