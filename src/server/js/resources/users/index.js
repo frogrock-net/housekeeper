f2388510eea6cc81015c70469788a77f2726eb01
alisazhou <alisa.zwy@gmail.com>
2018-11-23 21:20:21 -0500

    remove auth from user REST route

--- src/server/js/resources/users/index.js
+++ src/server/js/resources/users/index.js
@@ -1,17 +1,11 @@
 import express from 'express';
-import expressJwt from 'express-jwt';
 
 import UserModel from './model';
 import HouseModel from '../houses/model';
 
 const router = new express.Router();
 
-const auth = expressJwt({
-    secret: process.env.JWT_SECRET,
-    userProperty: 'payload',
-});
-
-router.get('/', auth, (req, res, next) => {
+router.get('/', (req, res, next) => {
     return UserModel.findById(req.payload.id, (err, user) => {
         if (err) {
             return next(err);
