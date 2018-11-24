041e127674ebb73e3a63a938ba027fd41789dd7b
alex benton <alex@frogrock.net>
2018-11-24 14:18:52 -0800

    merge commit incoming

--- src/client/config/dev.js
+++ src/client/config/dev.js
@@ -1,7 +1,8 @@
 /**
  * Defines a set of DEV configurations for this project.
  *
- * This file will OVERRIDE the default config.js file when deployed to the development environment.
+ * Do not depend on this file - use 'config.js' instead. This file will OVERRIDE the default config.js file when
+ * deployed to the development environment.
  */
 
 export const API_URL = 'https://housekeeper-223422.appspot.com/api';
--- src/client/js/app.jsx
+++ src/client/js/app.jsx
@@ -1,12 +1,21 @@
 // @flow
 import React from 'react';
 import Home from './components/Home/Home';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import * as config from '../config';
+
+const client = new ApolloClient(`${config.API_URL}/graphql`);
 
 /**
  * The highest-level component for the housekeeper application.
  *
  * @constructor
  */
-const App = () => <Home />;
+const App = () => (
+    <ApolloProvider client={client}>
+        <Home />
+    </ApolloProvider>
+);
 
 export default App;
