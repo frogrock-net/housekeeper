a389452ad1b5a6c45a531ddfefae22ba3ed42626
alex benton <alex@frogrock.net>
2018-11-24 12:18:07 -0800

    add some basic configuration

---
+++ src/client/config/dev.js
@@ -0,0 +1,7 @@
+/**
+ * Defines a set of DEV configurations for this project.
+ *
+ * This file will OVERRIDE the default config.js file when deployed to the development environment.
+ */
+
+export const API_URL = 'https://housekeeper-223422.appspot.com/api';
