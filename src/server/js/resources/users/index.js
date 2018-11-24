8f93d4667301962136381fcef9d40537a560f7ca
alisazhou <alisa.zwy@gmail.com>
2018-11-23 21:21:41 -0500

    revert to sending all users on list route

--- src/server/js/resources/users/index.js
+++ src/server/js/resources/users/index.js
@@ -6,12 +6,12 @@ import HouseModel from '../houses/model';
 const router = new express.Router();
 
 router.get('/', (req, res, next) => {
-    return UserModel.findById(req.payload.id, (err, user) => {
+    return UserModel.find({}, (err, users) => {
         if (err) {
             return next(err);
         }
 
-        res.send(user);
+        res.send(users);
     });
 });
 
