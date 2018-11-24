762af6d690562a46960fa6d51bb3289885a3202c
alex benton <alex@frogrock.net>
2018-11-23 18:29:02 -0800

    configure for google app engine.

--- src/server/js/server.js
+++ src/server/js/server.js
@@ -95,6 +95,7 @@ mongoose.connect(
     dbUrl,
     { useNewUrlParser: true }
 );
+
 mongoose.Promise = global.Promise;
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
